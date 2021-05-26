3f9b252f35c7f35c075990864280bd5bbc4c6a13
IsaacM890 <62258398+IsaacM890@users.noreply.github.com>
2021-05-26 21:34:07 +0300

    fix to https

--- src/api/config.js
+++ src/api/config.js
@@ -1,11 +1,12 @@
 /** @format */
 
 const API_KEY = 'WLPjmpTw1wszfd1kUnFEpglC8AQeQE7F';
+
 export const baseURL = {
-  autocompleteSearchURL: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
-  currentConditionsURL: `http://dataservice.accuweather.com/currentconditions/v1/`,
-  dailyForcastURL: `http://dataservice.accuweather.com/forecasts/v1/daily/1day/`,
-  hourlyForecastsURL: `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/`,
+  autocompleteSearchURL: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
+  currentConditionsURL: `https://dataservice.accuweather.com/currentconditions/v1/`,
+  dailyForcastURL: `https://dataservice.accuweather.com/forecasts/v1/daily/1day/`,
+  hourlyForecastsURL: `https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/`,
 };
 
 export const endPoints = {
